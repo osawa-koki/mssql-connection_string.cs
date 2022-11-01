c70a96fc541d4e86be9f9f23771ee762b05e9ddc
osawa-koki <osawa-koki@osawa.dev>
2022-11-01 10:26:17 +0000

    fst commit;;

---
+++ sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.SqlClient;
+
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new();
+sqlConnectionStringBuilder.DataSource = "localhost";
+sqlConnectionStringBuilder.UserID = "userid";
+sqlConnectionStringBuilder.Password = "password";
+sqlConnectionStringBuilder.InitialCatalog = "database";
+sqlConnectionStringBuilder.TrustServerCertificate = true;
+
+string connection_string = sqlConnectionStringBuilder.ConnectionString;
+
+Console.WriteLine(connection_string);
+
+// -> Data Source=localhost;Initial Catalog=database;User ID=userid;Password=password;TrustServerCertificate=True
